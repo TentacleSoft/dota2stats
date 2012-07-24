d22d0116ac4905e9b5391d84c26f04647dc2bbb4
Roger Clotet <clotet89@gmail.com>
2012-07-25 01:02:40 +0200

    A few style adjustments

--- src/app.php
+++ src/app.php
@@ -40,17 +40,17 @@ $app->get('/player/{account_id}/', function ($account_id) use ($app) {
 });
 
 $app->get('/match/{match_id}/', function ($match_id) use ($app) {
-    $url = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?match_id=' . $match_id . '&key=48F54125B3F7A12DE2F170FD65624598&account_id=18027978';
+    // $url = 'https://api.steampowered.com/IDOTA2Match_570/GetMatchDetails/V001/?match_id=' . $match_id . '&key=48F54125B3F7A12DE2F170FD65624598&account_id=18027978';
+//     
+    // $ch=curl_init();
+    // curl_setopt($ch, CURLOPT_URL, $url);
+    // curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
+    // $response = curl_exec ($ch);
+    // curl_close ($ch);
+//     
+    // $match = json_decode($response)->result;
     
-    $ch=curl_init();
-    curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, 1);
-    $response = curl_exec ($ch);
-    curl_close ($ch);
-    
-    $match = json_decode($response)->result;
-    
-    // $match = json_decode(file_get_contents("../data/match.json"))->result;
+    $match = json_decode(file_get_contents("../data/match.json"))->result;
     $items = json_decode(file_get_contents("../data/items.json"), true);
     $heroes = json_decode(file_get_contents("../data/heroes.json"), true);
     
